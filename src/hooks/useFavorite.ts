538b955aaed6f23a59536600fb1ae0420d07fec5
Stanisław Synowiec <stasyn1410@gmail.com>
2026-03-11 22:32:45 +0100

    chore: use favorite cocktails hook use jotai atom with storage

---
+++ src/hooks/useFavorite.ts
@@ -0,0 +1,21 @@
+import { atom, useAtom, useSetAtom } from "jotai";
+import { atomWithStorage } from "jotai/utils";
+
+const favoritesAtom = atomWithStorage<number[]>("favorites", []);
+
+const toggleFavoriteAtom = atom(null, (get, set, id: number) => {
+  const favorites = get(favoritesAtom);
+  if (favorites.includes(id))
+    set(
+      favoritesAtom,
+      favorites.filter((f) => f !== id),
+    );
+  else set(favoritesAtom, [...favorites, id]);
+});
+
+export const useFavorite = () => {
+  const [favorites] = useAtom(favoritesAtom);
+  const toggleFavorite = useSetAtom(toggleFavoriteAtom);
+
+  return { favorites, toggleFavorite };
+};
