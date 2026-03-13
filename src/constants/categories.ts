409ce595e5c125eb8cd213f17796ee8c3b00c90e
Stanisław Synowiec <stasyn1410@gmail.com>
2026-03-13 22:17:24 +0100

    chore: categories const

---
+++ src/constants/categories.ts
@@ -0,0 +1,12 @@
+export const categories = [
+  "Cocktail",
+  "Ordinary Drink",
+  "Punch / Party Drink",
+  "Shake",
+  "Other / Unknown",
+  "Cocoa",
+  "Shot",
+  "Coffee / Tea",
+  "Homemade Liqueur",
+  "Soft Drink",
+];
