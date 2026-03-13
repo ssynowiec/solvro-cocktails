c591cbc2aec0144a6b53cc91a36b38cbce6e8520
Stanisław Synowiec <stasyn1410@gmail.com>
2026-03-13 12:51:55 +0100

    fix: add suspense

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
+import { Suspense } from "react";
+
 import { CocktailsList } from "@/components/cocktails-list";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <CocktailsList />
+      <Suspense>
+        <CocktailsList />
+      </Suspense>
       {/*<CocktailsPagination />*/}
     </main>
   );
