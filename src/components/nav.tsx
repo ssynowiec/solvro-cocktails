21506f7ee64b46a06db151f982d48d0144ebc195
Stanisław Synowiec <stasyn1410@gmail.com>
2026-03-15 15:28:21 +0100

    chore: add logo to nav

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -1,9 +1,13 @@
+import Image from "next/image";
+
+import logo from "@/assets/logo.png";
 import { MobileFilters } from "@/components/mobile-filters";
 import { ThemeToggle } from "@/components/theme-toggle";
 
 export function Nav() {
   return (
     <nav className="bg-background sticky top-0 z-10 flex h-12 items-center gap-4 px-4">
+      <Image src={logo} alt="Cocktail" className="h-10 w-10" />
       <h1>Cocktails KN Solvro</h1>
       <div className="ml-auto">
         <ThemeToggle />
