5e97c1bda10dc1ebf6d0b33c014f15b4ace74e86
Stanisław Synowiec <stasyn1410@gmail.com>
2026-03-15 15:35:46 +0100

    chore: per page select

--- src/components/cocktails-pagination.tsx
+++ src/components/cocktails-pagination.tsx
@@ -7,6 +7,14 @@ import {
   PaginationNext,
   PaginationPrevious,
 } from "@/components/ui/pagination";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
 import { usePage } from "@/hooks/use-page";
 
 interface CocktailsPaginationProps {
@@ -16,25 +24,45 @@ interface CocktailsPaginationProps {
 }
 
 export function CocktailsPagination({
-  perPage,
   total,
   lastPage,
 }: CocktailsPaginationProps) {
-  const { page, setPage } = usePage();
+  const { page, setPage, perPage, handleChangePerPage } = usePage();
 
   return (
     <div className="flex w-full flex-col items-center gap-4 pt-6 sm:flex-row sm:justify-between">
-      <p className="text-muted-foreground w-full text-center text-sm md:w-full md:text-left">
-        Showing{" "}
-        <span className="text-foreground font-medium">
-          {(page - 1) * perPage + 1}
-        </span>{" "}
-        -{" "}
-        <span className="text-foreground font-medium">
-          {page === lastPage ? total : page * perPage}
-        </span>{" "}
-        from <span className="text-foreground font-medium">{total}</span>{" "}
-      </p>
+      <div className="flex w-full items-center gap-4">
+        <p className="text-muted-foreground w-auto text-center text-sm md:text-left">
+          Showing{" "}
+          <span className="text-foreground font-medium">
+            {(page - 1) * perPage + 1}
+          </span>{" "}
+          -{" "}
+          <span className="text-foreground font-medium">
+            {page === lastPage ? total : page * perPage}
+          </span>{" "}
+          from <span className="text-foreground font-medium">{total}</span>{" "}
+        </p>
+        <div className="flex items-center gap-2">
+          <p className="text-muted-foreground text-sm md:text-left">per page</p>
+          <Select
+            defaultValue={perPage.toString()}
+            onValueChange={async (value) => handleChangePerPage(Number(value))}
+          >
+            <SelectTrigger>
+              <SelectValue placeholder="Items per page" />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectGroup>
+                <SelectItem value="15">15</SelectItem>
+                <SelectItem value="30">30</SelectItem>
+                <SelectItem value="50">50</SelectItem>
+                <SelectItem value="100">100</SelectItem>
+              </SelectGroup>
+            </SelectContent>
+          </Select>
+        </div>
+      </div>
 
       <Pagination>
         <PaginationContent>
